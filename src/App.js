d9d65c7b64dd6abaa9d92e07f296dd085e306afc
COllins <collinsmuiruri77@gmail.com>
2023-01-20 17:53:38 +0300

    Created signup.js and signup.css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [storedToken, setStoredToken] = useState(localStorage.getItem("token"));
+  const [role, setRole] = useState("");
+  const [loggedInUserId, setLoggedInUserId] = useState("");
+
+  useEffect(() => {
+    fetch("/api/v1/profile ", {
+      method: "GET",
+      headers: {
+        Accepts: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.token}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setRole(data.user.role);
+        setLoggedInUserId(data.user.id);
+      });
+  }, [storedToken]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    {storedToken ? (
+      <Router>
+        {role === "admin"? <AdminApp setStoredToken={setStoredToken}/> : 
+        <>
+        <Navbar setStoredToken={setStoredToken}/>
+        <Routes>
+        </Routes>
+        <Footer /> 
+        </>}
+        
+      </Router>
+    ) : (
+      <div>
+      <Router>
+      <Routes>
+        <Route path="/login" element={<Login setStoredToken={setStoredToken}/>} exact/>
+        <Route path="/signup" element={<Signup setStoredToken={setStoredToken}/>} exact/>
+      </Routes>
+      </Router>
+      </div>
+    )}
+  </div>
   );
 }
 
